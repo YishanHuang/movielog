2bd7893cdf2ebd41c22afd0731222887b92a207e
YishanHuang <hyishan@hawk.iit.edu>
2015-03-08 16:51:38 -0500

    add comment model and link two models together

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ActiveRecord::Base
+    has_many :comments, dependent: :destroy
     validates :name, presence:true,
     length: { minimum: 1, maximum: 30,
     too_long: "%{count} characters is the maximum allowed" }
