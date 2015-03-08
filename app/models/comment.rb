4b64f0a8694a5fe8eefc25bfb16081d179c5885a
YishanHuang <hyishan@hawk.iit.edu>
2015-03-08 17:31:46 -0500

    add few test units

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :movie
+  validates :name, presence:true,
+  length: { minimum: 1, maximum: 30,
+  too_long: "%{count} characters is the maximum allowed" }
 end
