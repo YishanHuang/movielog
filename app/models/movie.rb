4ba829a6e86dba4318717ca8eb241f11e515cd9c
YishanHuang <hyishan@hawk.iit.edu>
2015-04-11 16:03:54 -0500

    tried to change the default date in seeds.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,10 +3,10 @@ class Movie < ActiveRecord::Base
     validates :name,  presence:true,
     length: { minimum: 1, maximum: 30,
     too_long: "%{count} characters is the maximum allowed" }
-    validate :date_cannot_be_in_the_future, on: :create
+   # validate :date_cannot_be_in_the_future, on: :create
 
     def date_cannot_be_in_the_future
-        if :date > Date.today
+        if  date > Date.today
             errors.add(:date, "can't be in the future")
         end
     end
